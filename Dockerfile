a4b6a326e820d47196bc55c814e57b8c33178c2f
Roberto Contreras <contreras.java@gmail.com>
2025-08-15 02:19:11 -0500

    demo

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM alpine/java:21-jdk
+
+WORKDIR /app
+
+COPY target/demo-dockerizar-ms-0.0.1-SNAPSHOT.jar app.jar
+
+EXPOSE 8090
+
+ENTRYPOINT ["java", "-jar", "app.jar"]
---
+++ src/main/java/com/demo/DemoDockerizarMsApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDockerizarMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDockerizarMsApplication.class, args);
+	}
+
+}
